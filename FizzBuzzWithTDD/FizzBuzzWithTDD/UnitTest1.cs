6a7beb9f35f4a9e328db17e8bb5c7dc9098b0740
Meri Eskelinen <meri.eskelinen@visma.com>
2021-05-04 14:55:14 +0300

    Initial commit

---
+++ FizzBuzzWithTDD/FizzBuzzWithTDD/UnitTest1.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace FizzBuzzWithTDD
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.Pass();
+        }
+    }
+}
